e7c357b6546ba4de4b7a6a1767531d9e84c1dc2f
Oksana <myzove@mail.ru>
2020-08-19 20:20:44 +0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
- [LinkedIn Chat](https://oksanacoder.github.io/linkedIn-chat-react-socket/).
-
-Used tools:
-- socket.io-client
-- React
-- react hooks
-- emoji-mart
-- react-fontawesome
---
+++ precache-manifest.ae8a7aaf4e4d6b0870b2853b0596616f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273ba23eed1b78528c652a4a726cc80e",
+    "url": "/linkedIn-chat-react-socket/index.html"
+  },
+  {
+    "revision": "4f1943261ad06beeb8f9",
+    "url": "/linkedIn-chat-react-socket/static/css/2.ddf7986b.chunk.css"
+  },
+  {
+    "revision": "16a7e0182e05a47e86e4",
+    "url": "/linkedIn-chat-react-socket/static/css/main.37d63ae3.chunk.css"
+  },
+  {
+    "revision": "4f1943261ad06beeb8f9",
+    "url": "/linkedIn-chat-react-socket/static/js/2.b82c8845.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/linkedIn-chat-react-socket/static/js/2.b82c8845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9abe9c34495fd1eb910",
+    "url": "/linkedIn-chat-react-socket/static/js/3.234dcc88.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/linkedIn-chat-react-socket/static/js/3.234dcc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a7e0182e05a47e86e4",
+    "url": "/linkedIn-chat-react-socket/static/js/main.d78b9912.chunk.js"
+  },
+  {
+    "revision": "d0a4838c58c964ce9256",
+    "url": "/linkedIn-chat-react-socket/static/js/runtime-main.8f9c9a4d.js"
+  },
+  {
+    "revision": "4875896c13ef6b4bb1bcfab557b833c8",
+    "url": "/linkedIn-chat-react-socket/static/media/1111.4875896c.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/linkedIn-chat-react-socket/static/media/logo.5d5d9eef.svg"
+  }
+]);
